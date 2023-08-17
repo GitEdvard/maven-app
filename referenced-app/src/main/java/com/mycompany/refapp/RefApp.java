aad62b129a6621297197216b5a2480cdf01ef82d
Edvard Englund <edvard.englund@medsci.uu.se>
2023-08-17 10:53:21 +0200

    Add referenced project

---
+++ referenced-app/src/main/java/com/mycompany/refapp/RefApp.java
@@ -0,0 +1,13 @@
+package com.mycompany.refapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class RefApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from referenced!" );
+    }
+}
